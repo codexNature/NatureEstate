545d9f725edb89a1dfc3506b17e4434c6b5e63d0
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-10-17 01:01:20 +0100

    all pages setup and rendering good

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './Pages/Signin';
+import SignUp from './Pages/SignUp';
+import About from './Pages/About';
+import Profile from './pages/Profile';
+import Header from './Components/Header';
+
 
 export default function App() {
   return (
-    <div className='text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
