76a6b383b3f32fdad858e2fccf484d6dac36ef5c
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-10-20 04:32:16 +0100

    add redux persist

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  useReducer  from './user/userSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import  userReducer  from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({user: userReducer})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+}
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {user: useReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => 
   getDefaultMiddleware({
     serializableCheck: false
   }),
 });
+
+export const persistor = persistStore(store);
+
+//persistor, persistGate, persistReducer all are used for redux persist and redux persist is used for storing the data in local storage so that when we refresh the page the data is not lost.
