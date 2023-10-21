ce749c9581a9e98368bbaa6ea44d09be12a247ec
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-10-21 04:42:34 +0100

    complete profile page UI

--- Client/src/Components/PrivateRoute.jsx
+++ Client/src/Components/PrivateRoute.jsx
@@ -5,7 +5,4 @@ export default function PrivateRoute() {
   const { currentUser } = useSelector(state => state.user);
   return currentUser ? <Outlet /> : <Navigate to= '/signin'/> // This will redirect the user to the signin page if they are not authenticated.
     
-    //<img className=' rounded-full h-15 w-15 object-cover' src={currentUser.avatar} alt="Profile" />
-    
-  
-}
+  }
