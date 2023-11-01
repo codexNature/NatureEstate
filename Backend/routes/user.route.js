a1535f20a35ff520bcb07265009dea3a7f261c58
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-11-01 03:16:04 +0100

    create get user lisitings api route

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser,getUserLisitings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserLisitings);
 
 export default router;
