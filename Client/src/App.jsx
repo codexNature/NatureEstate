ac42d281839a131a890e37aac77d257feadb92a6
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-11-13 19:15:05 +0100

    edit app.jsx signin

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import SignIn from './Pages/SignIn';
+import SignIn from './Pages/Signin';
 import SignUp from './Pages/SignUp';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
